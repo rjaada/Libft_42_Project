396098a8303a6f4cc089b053fbee3a845c51b8c4
Rachid Jaada <124290716+rjaada@users.noreply.github.com>
2024-05-23 16:19:33 +0200

    Add comments for readability

--- ft_atoi.c
+++ ft_atoi.c
@@ -1,48 +1,11 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_atoi.c                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: rjaada <rjaada@student.42.fr>              +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/13 15:46:24 by rjaada            #+#    #+#             */
-/*   Updated: 2023/10/19 19:22:26 by rjaada           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include "libft.h"
-
-//Convert a string to an integer.
-//This function takes a string as input and converts it into an integer.
-//It handles leading whitespace characters, 
-//positive and negative signs, and a sequence of digits
-//to produce the final integer result.
-//Parameters:
-//- str: The input string to be converted to an integer.
-//Returns:
-//The converted integer.
-int	ft_atoi(const char *str)
-{
-	long			res;
-	long			sign;
-	unsigned int	i;
-
-	res = 0;
-	sign = 1;
-	i = 0;
-	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'
-		|| str[i] == '\r' || str[i] == '\v' || str[i] == '\f')
-		i++;
-	if (str[i] == '-' || str[i] == '+')
-	{
-		if (str[i] == '-')
-			sign = -1;
-		i++;
-	}
-	while (str[i] >= '0' && str[i] <= '9')
-	{
-		res = res * 10 + str[i] - '0';
-		i++;
-	}
-	return ((int)(res * sign));
-}
+/*
+                                                                                                                                           * */
+/*
+                                                                                                                              *  */
+/*
+                                                                                                                                
+                                            ft_atoi.c                                                                                       
+                                                                                                                                   
+/*
+                                                                                                                                                  
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
